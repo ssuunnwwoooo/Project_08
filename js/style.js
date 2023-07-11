d65ea2fd9a5d1b9589a2d06462a10953c478fa4f
ssuunnwwoooo <o3olia@naver.com>
2023-07-11 18:12:20 +0900

    230711

--- js/style.js
+++ js/style.js
@@ -21,20 +21,50 @@ $(function () {
 
     var mainslide = new Swiper(".main_slide", {
         loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            renderBullet: function (index, className) {
+                return '<span class="' + className + '">' + (index + 1) + "</span>";
+            },
+        },
     });
 
-    // $('.main_menu .arrows .left').on('click', function () {
-    //     menuSlide.slidePrev();
-    // });
-    // $('.main_menu .arrows .right').on('click', function () {
-    //     menuSlide.slideNext();
-    // });
+    /*   $('.main_visual .arrows .left').on('click', function () {
+           mainslide.slidePrev();
+        });
+         $('.main_visual .arrows .right').on('click', function () {
+           menuSlide.slideNext();
+       }); */
 
-    var contentslide = new Swiper(".content_slide", {
-        slidesPerView: 4,
-        spaceBetween: 30,
-        loop: true,
 
+    $('.content_slide').slick({
+        slidesToShow: 4,
+        slidesToScroll: 2,
+        prevArrow: $('.left'),
+        nextArrow: $('.right'),
     });
 
+    var pointSize = $(".circle").width() / 2;
+    $(".content_wrap").mousemove(function (e) {
+        $('.circle').css("top", e.pageY - pointSize);
+        $('.circle').css("left", e.pageX - pointSize);
+        $('.circle').fadeIn();
+    });
+    $(".content_wrap").on("mouseleave", function () {
+        $('.circle').fadeOut();
+    });
+
+    $(document).ready(function () {
+        $('ul.tabs li').click(function () {
+            var tab_id = $(this).attr('data-tab');
+
+            $('ul.tabs li').removeClass('current');
+            $('.tab-content').removeClass('current');
+
+            $(this).addClass('current');
+            $("#" + tab_id).addClass('current');
+        });
+
+    });
 });
